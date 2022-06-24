ad2d6d432f09230bf87277d4b69718b1ca601f00
haythembensouissi <haythembensouissi2@gmail.com>
2022-06-24 18:35:51 +0100

    work in progress

--- client/components/oneadd.jsx
+++ client/components/oneadd.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 const Oneadd=(props)=>{
     <div className="oneTodo">
-  <h1>{props.content.countryname}</h1>
-  <img src={props.content.image}/>
+  <h1 className="header">{props.content.countryname}</h1>
+  <img className="country" src={props.content.image}/>
     <p className="paragraph">{props.content.description}</p>
 </div>
 }
--- server/server.js
+++ server/server.js
@@ -2,12 +2,16 @@ var express=require("express")
 const handle=require("../server/handlers/index")
 const app=express()
 app.use(express.json())
-const port=5000
 var host="127.0.0.1"
 app.get("/user/:id",handle.getaUser)
+
 app.post("/flight",handle.addaflight)
+
 app.put("/flight/:id",handle.updateaflightDate)
+
 app.delete("/flight/:id",handle.deleteaflight)
+
+const port=5000
 app.listen(port,()=>{
     console.log(`listening on http://${host}:${port}`)
 })
